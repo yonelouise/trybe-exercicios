a213304abbcbe62a84200baf9bf8b605226562ac
Yone Louise <yone_louise@hotmail.com>
2022-03-10 13:13:42 -0300

    ex001-004

---
+++ Fundamentos/[ ] 4 - Introdução JavaScript/Dia-1/ex001-a.js
@@ -0,0 +1,8 @@
+//Adição
+
+const a = 10;
+const b = 5;
+
+let resultado = a + b;
+
+console.log(resultado);
---
+++ Fundamentos/[ ] 4 - Introdução JavaScript/Dia-1/ex001-c.js
@@ -0,0 +1,8 @@
+// Multiplicação
+
+const a = 10;
+const b = 5;
+
+let resultado = a * b;
+
+console.log(resultado);
