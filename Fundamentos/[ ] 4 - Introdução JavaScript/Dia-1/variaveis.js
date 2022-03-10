1e6f36b0dd78b27b6fcfd7dbc57774abe6d2b0ce
Yone Louise <yone_louise@hotmail.com>
2022-03-09 22:53:26 -0300

    corrigindo nomes de pastas

---
+++ Fundamentos/[ ] 4 - Introdução JavaScript/Dia-1/variaveis.js
@@ -0,0 +1,9 @@
+
+const myName = "Yone";
+const birthCity = "Massaranduba";
+let birthYear = 1996;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
---
+++ Fundamentos/[ ] 4 - Introdução JavaScript/Dia-3/ex-aula-01.js
@@ -0,0 +1,12 @@
+// 1. Faça um algoritmo que calcule a soma de 50 a 100 usando a estrutura “for” e retorne no formato:
+// A soma de 50 a 100 é: X.
+
+let numero = [50];
+let entrada = 0;
+
+for (let index = 0; index < numero.length; index++){
+  entrada = numero++;
+}
+
+console.log (numero);
+
