1ffca7e8c37d2efff019e9ef0d24ff0b05f336dd
Yone Louise <yone_louise@hotmail.com>
2022-03-09 23:16:04 -0300

    removendo pastas dias

--- Fundamentos/[ ] 4 - Introdução JavaScript/Dia-3/ex-aula-02.js
+++
@@ -1,8 +0,0 @@
-// 2. Crie um algoritmo que conte quantos números do intervalo entre 1 e 125 são divisíveis por 3.
-// Caso a quantidade seja igual a 50, exiba uma mensagem secreta a sua escolha.
-// 3. Crie um algoritmo que simula o jogo “pedra, papel e tesoura” levando em consideração que são apenas duas pessoas jogando e imprima o resultado no formato:
-// “Jogador 1 vence” ou “Empate” ou “Jogador 2 vence”.
-// 4. Desenvolva um algoritmo que verifica se a pessoa é maior ou menor de idade.
-// Imprima no console seguindo o exemplo: “A pessoa é maior de idade”.
-// 5. Crie um algoritmo que recebe a idade de Marcella, Carlos e Camila e imprima quem é a mais nova no formato:
-// “Pessoa” é a mais nova.
